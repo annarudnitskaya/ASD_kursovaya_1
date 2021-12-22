6e35678806c67309d0a80fe388c4f044ed894eee
annarudnitskaya <75776048+annarudnitskaya@users.noreply.github.com>
2021-12-22 20:10:18 +0300

    Update UnitTest_ASD_kursovaya_1.cpp

--- UnitTest_ASD_kursovaya_1.cpp
+++ UnitTest_ASD_kursovaya_1.cpp
@@ -12,154 +12,154 @@ namespace UnitTestASD_kursovaya_1
 
 		TEST_METHOD(Testcheck_for_correct_input)
 		{
-			string stroka = "8 + 9(*";
+			string str = "8 + 9(*";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of application *", error.what());
 			}
-			stroka = "\0";
-			stroka = "(+8 + 9";
+			str = "\0";
+			str = "(+8 + 9";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of application +", error.what());
 			}
-			stroka = "\0";
-			stroka = "8 + 9(-";
+			str = "\0";
+			str = "8 + 9(-";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of application -", error.what());
 			}
-			stroka = "\0";
-			stroka = "(/ 8 + 9";
+			str = "\0";
+			str = "(/ 8 + 9";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of application /", error.what());
 			}
 			
-			stroka = "\0";
-			stroka = "8 + 9^)";
+			str = "\0";
+			str = "8 + 9^)";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of application ^", error.what());
 			}
-			stroka = "\0";
-			stroka = "aoc(-4)";
+			str = "\0";
+			str = "aoc(-4)";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of the abs function", error.what());
 			}
-			stroka = "\0";
-			stroka = "son(-4)";
+			str = "\0";
+			str = "son(-4)";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of the sin or sqrt function", error.what());
 			}
-			stroka = "\0";
-			stroka = "socu(4)";
+			str = "\0";
+			str = "socu(4)";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of the sin or sqrt function", error.what());
 			}
-			stroka = "\0";
-			stroka = "cin(-4)";
+			str = "\0";
+			str = "cin(-4)";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of the cos or ctg function", error.what());
 			}
-			stroka = "\0";
-			stroka = "cos4)";
+			str = "\0";
+			str = "cos4)";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of arithmetics the cos or ctg function", error.what());
 			}
-			stroka = "\0";
-			stroka = "ctg4)";
+			str = "\0";
+			str = "ctg4)";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of arithmetics the cos or ctg function", error.what());
 			}
-			stroka = "\0";
-			stroka = "lon(10)";
+			str = "\0";
+			str = "lon(10)";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of the log or ln function", error.what());
 			}
-			stroka = "\0";
-			stroka = "log10)";
+			str = "\0";
+			str = "log10)";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect arithmetics the log function", error.what());
 			}
-			stroka = "\0";
-			stroka = "ln-e)";
+			str = "\0";
+			str = "ln-e)";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect arithmetics the ln function", error.what());
 			}
-			stroka = "\0";
-			stroka = "ta(-4)";
+			str = "\0";
+			str = "ta(-4)";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of the tg function", error.what());
 			}
-			stroka = "\0";
-			stroka = "tg45)";
+			str = "\0";
+			str = "tg45)";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect arithmetics the tg function", error.what());
 			}
-			stroka = "\0";
-			stroka = "pa";
+			str = "\0";
+			str = "pa";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect input of the pi const", error.what());
 			}
-			stroka = "\0";
-			stroka = "e3";
+			str = "\0";
+			str = "e3";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Incorrect arithmetics the e const", error.what());
 			}
-			stroka = "\0";
-			stroka = "8!";
+			str = "\0";
+			str = "8!";
 			try {
-				check_for_correct_input(stroka);
+				check_for_correct_input(str);
 			}
 			catch (std::invalid_argument error) {
 				Assert::AreEqual("Invalid character. The calculator can use numbers like double, brackets, actions: +, -, *, /, ^ and functions: sin(), cos(), tg(), ctg(), sqrt(), log(), ln(), abs()", error.what());
@@ -167,71 +167,71 @@ namespace UnitTestASD_kursovaya_1
 		}
 		TEST_METHOD(TesthasPrecedence)
 		{
-			string stroka = "\0";
-			stroka = "-";
-			Assert::IsTrue(hasPrecedence(stroka) == 1);
+			string str = "\0";
+			str = "-";
+			Assert::IsTrue(hasPrecedence(str) == 1);
 
-			stroka = "\0";
-			stroka = "+";
-			Assert::IsTrue(hasPrecedence(stroka) == 1);
+			str = "\0";
+			str = "+";
+			Assert::IsTrue(hasPrecedence(str) == 1);
 
-			stroka = "\0";
-			stroka="*";
-			Assert::IsTrue(hasPrecedence(stroka) == 2);
+			str = "\0";
+			str="*";
+			Assert::IsTrue(hasPrecedence(str) == 2);
 
-			stroka = "\0";
-			stroka = "/";
-			Assert::IsTrue(hasPrecedence(stroka) == 2);
+			str = "\0";
+			str = "/";
+			Assert::IsTrue(hasPrecedence(str) == 2);
 			
-			stroka = "\0";
-			stroka = "^";
-			Assert::IsTrue(hasPrecedence(stroka) == 3);
+			str = "\0";
+			str = "^";
+			Assert::IsTrue(hasPrecedence(str) == 3);
 
-			stroka = "\0";
-			stroka = "sin";
-			Assert::IsTrue(hasPrecedence(stroka) == 4);
+			str = "\0";
+			str = "sin";
+			Assert::IsTrue(hasPrecedence(str) == 4);
 
-			stroka = "\0";
-			stroka = "cos";
-			Assert::IsTrue(hasPrecedence(stroka) == 4);
+			str = "\0";
+			str = "cos";
+			Assert::IsTrue(hasPrecedence(str) == 4);
 
-			stroka = "\0";
-			stroka = "sqrt";
-			Assert::IsTrue(hasPrecedence(stroka) == 4);
+			str = "\0";
+			str = "sqrt";
+			Assert::IsTrue(hasPrecedence(str) == 4);
 
-			stroka = "\0";
-			stroka = "tg";
-			Assert::IsTrue(hasPrecedence(stroka) == 4);
+			str = "\0";
+			str = "tg";
+			Assert::IsTrue(hasPrecedence(str) == 4);
 
-			stroka = "\0";
-			stroka = "ctg";
-			Assert::IsTrue(hasPrecedence(stroka) == 4);
+			str = "\0";
+			str = "ctg";
+			Assert::IsTrue(hasPrecedence(str) == 4);
 
-			stroka = "\0";
-			stroka = "abs";
-			Assert::IsTrue(hasPrecedence(stroka) == 4);
+			str = "\0";
+			str = "abs";
+			Assert::IsTrue(hasPrecedence(str) == 4);
 
-			stroka = "\0";
-			stroka = "log";
-			Assert::IsTrue(hasPrecedence(stroka) == 4);
+			str = "\0";
+			str = "log";
+			Assert::IsTrue(hasPrecedence(str) == 4);
 
-			stroka = "\0";
-			stroka = "ln";
-			Assert::IsTrue(hasPrecedence(stroka) == 4);
+			str = "\0";
+			str = "ln";
+			Assert::IsTrue(hasPrecedence(str) == 4);
 		}
 		TEST_METHOD(Testpostfix)
 		{
-			string stroka("sin(30) + 3 * 4");
-			string postcheck = postfix(stroka);
+			string str("sin(30) + 3 * 4");
+			string postcheck = postfix(str);
 			string check("30 sin 3 4 * +");
 			Assert::IsTrue(check == postcheck);
 		}
 
 		TEST_METHOD(Testcalculate)
 		{
-			string stroka("0.5 3 4 * +");
+			string str("0.5 3 4 * +");
 			double check = 12.5;
-			Assert::IsTrue(check == calculate(stroka));
+			Assert::IsTrue(check == calculate(str));
 		}
 	};
 }
